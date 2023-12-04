09952a346f5766aca376002ccf95123884601197
Akif Albayrak <akifalbayrak6161@gmail.com>
2023-12-04 23:55:22 +0300

    Initial commit

---
+++ frontend/src/pages/NewEvent.js
@@ -0,0 +1,9 @@
+import EventForm from "../components/EventForm";
+
+function NewEventPage(){
+    function submitHandler(event){
+        event.preventDefault();
+    }
+    return <EventForm method="post"/>
+}
+export default NewEventPage;
